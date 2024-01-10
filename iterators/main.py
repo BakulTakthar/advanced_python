2213d2efaf1728f6d8eb2800adfb3bc6e1c9aab4
Bakul Takthar <bakul184bsingh@gmail.com>
2024-01-10 22:03:46 +0530

    iterators theory day2

--- iterators/main.py
+++ iterators/main.py
@@ -40,4 +40,20 @@ def iteration_using_next():
         line = next(open1)
         print(f"{line_num} | {line}")
 
-iteration_using_next()
+#iteration_using_next()   # <- run this for the above mentioned method
+        
+"""
+Theory of itertators 
+
+the iteration classification of object is done on two basis 1) iteration tool and 2) iterator object
+
+1) iteration tool/conetxt - the tools and objects or conditions which make the iteration possible in the 
+'iterable object' (does itertaion on an object which allows iteration).
+for example for, map, comprehension etc.
+
+2) iterator object - the object returned by the iteration on the iterable object
+(the output retured by oone itertaion on the iterable object by the iteration tool/ context)
+"""
+
+#*? sometimes the iterator object (the output object of the iteration tool/context on the iterable object) is an iterable object as well (allows for iteration)
+#*? eg- itertaion on a nested list returns another list (which by being a list itself, allows for itertion) is an iterable object.
