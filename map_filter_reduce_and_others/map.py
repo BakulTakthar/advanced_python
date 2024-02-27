7462b0db73b48c2645c97222a6d5012bc3dca2c1
Bakul Takthar <bakul184bsingh@gmail.com>
2024-02-27 16:27:49 +0530

    added reduce_function

--- map_filter_reduce_and_others/map.py
+++ map_filter_reduce_and_others/map.py
@@ -1,19 +1,6 @@
-'''
-#? to get a grasp of map() function in python
-'''
+array = ['a', 'e', 'i', 'o', 'u']
 
-example = [1,2,3,4,5,6,7,8,9,10]
+alphabets = ['a', 'b', 'c', 'd', 'e']
 
-'''
-#*- lets say that we need to add one to each of the element of this list
-'''
-#* without map() function
-
-for i in range(len(example)):
-    example[i] = example[i] + 1
-    
-print(example)
-
-#* WITH THE MAP FUNCTION
-example = map(lambda x: x + 1, example)
-print(list(example))
+result = filter(lambda x: x in array, alphabets)
+print(list(result))
